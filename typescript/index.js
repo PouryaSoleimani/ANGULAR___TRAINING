6fb2157b1d5dc9387b74e8736b7f9b52c1bd8533
Pourya_Delivando <Pourya@delivando.com>
2025-05-17 23:51:01 +0330

    ANGULAR____________

--- typescript/index.js
+++ typescript/index.js
@@ -1,9 +1,10 @@
 console.log('hello world');
 function myNameLogger(name) {
-    console.log(name);
+  console.log(name);
 }
+
 myNameLogger('Pourya Soleimani');
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 console.log(add(5, 2));
